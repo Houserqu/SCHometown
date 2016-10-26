23f8db891c6c9ebf742593d68ad71efaafc0c2a6
Houser <houserqu@qq.com>
2016-10-27 00:29:18 +0800

    微信 signature 校验

--- routes/loginRouter.js
+++ routes/loginRouter.js
@@ -2,30 +2,11 @@ var express = require('express');
 var router = express.Router();
 
 var wechatconfig = require("../config/wechat");
-//var sha1 = require("../config/sha1");
 var pool = require("../config/mysql");
 
 
 //检验signature
-router.get('/checkSignature',function (req, res, next) {
-    var signature = req.params.signature;
-
-    var timestamp = req.params.timestamp;
-    var nonce = req.params.nonce;
-    var echostr = req.params.echostr;
-
-    console.log(echostr);
-
-    // var arr = [wechatconfig.Token, timestamp, nonce];
-    // arr.sort();
-    //
-    // var str = arr[0] + arr[1] + arr[2];
-    //
-    // if(signature == sha1(str)){
-    //     res.send(echostr)
-    // }
-    res.send(echostr);
-
+router.get('/',function (req, res, next) {
 
 });
 
