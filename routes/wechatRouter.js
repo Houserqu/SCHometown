d7c91484d363ff7b58624804950129ad8b2299e1
Houser <houserqu@qq.com>
2016-10-27 17:07:08 +0800

    微信授权登录

--- routes/wechatRouter.js
+++ routes/wechatRouter.js
@@ -1,11 +1,16 @@
 var express = require('express');
 var request = require("request");
-var router = express.Router();
-
-var wechatconfig = require("../config/wechat");
 var pool = require("../config/mysql");
 
+var router = express.Router();
 
+var pool = require("../config/mysql");
+
+var wechatconfig = {
+    appid:"wx86caab40dba425ba",
+    appsecret:"d4624c36b6795d1d99dcf0547af5443d",
+    Token:"houser"
+}
 router.get('/responMsg', function (req, res, next) {
 
 });
@@ -18,29 +23,48 @@ router.get("/login", function (req, res, next) {
     console.log("state:" + state);
 
     //微信获取用户资料第二步通过code换取网页授权access_token
-    var url = "https://api.weixin.qq.com/sns/oauth2/access_token?appid=wx86caab40dba425ba&secret=d4624c36b6795d1d99dcf0547af5443d&code=" + code + "&grant_type=authorization_code"
-    request.get({url:url ,form:{}}, function (error, response, body) {
-        if (!error && response.statusCode == 200) {
-            console.log(body);
-            var jsondata = JSON.parse(body);
+    getAccessToken(wechatconfig.appid, wechatconfig.appsecret,code,function (err, accesstoken) {
 
-            //第四步拉取用户信息(需scope为 snsapi_userinfo)
-            request.get({url:"https://api.weixin.qq.com/sns/userinfo?access_token="+jsondata.access_token+"&openid="+jsondata.openid+"&lang=zh_CN ",form:{}}, function (error, response, body) {
-                if (!error && response.statusCode == 200) {
-                    var userdata = JSON.parse(body);
-                    console.log(userdata);
-                    res.end(JSON.stringify(userdata));
-                }else{
-                    res.end("get userinfo error");
-                }
-            });
-        }else{
-            res.end("get access_token error");
-        }
+        getUserinfo(accesstoken.access_token, accesstoken.openid,function (err, userinfo) {
+            res.send(userinfo);
+        });
     });
 });
 
-var getCode = function (qppid, encodeurl) {
-    var url = "https://open.weixin.qq.com/connect/oauth2/authorize?appid=wx86caab40dba425ba&redirect_uri=http%3a%2f%2fwechat.itwang.wang%2fwechat%2flogin&response_type=code&scope=snsapi_userinfo&state=STATE#wechat_redirect"
+var userExist = function (openid, cb) {
+    pool.getConnection(function (err, conn) {
+        if(err) throw err;
+        conn.query('select * from user where openid = ?',openid, function (err,result) {
+            conn.release();
+            if(err) throw err;
+            if(result[0].affectedRows){
+                cb(err, 1);
+            }else{
+                cb(err, 0);
+            }
+        });
+    });
+}
+var url = "https://open.weixin.qq.com/connect/oauth2/authorize?appid=wx86caab40dba425ba&redirect_uri=http%3a%2f%2fwechat.itwang.wang%2fwechat%2flogin&response_type=code&scope=snsapi_userinfo&state=STATE#wechat_redirect"
+
+
+var getAccessToken = function (appid, secret, code, cb) {
+    //微信获取用户资料第二步通过code换取网页授权access_token
+    var url = "https://api.weixin.qq.com/sns/oauth2/access_token?appid="+appid+"&secret="+secret+"&code=" + code + "&grant_type=authorization_code"
+    request.get({url:url ,form:{}}, function (error, response, body) {
+        if (!error && response.statusCode == 200) {
+            var jsondata = JSON.parse(body);
+            cb(err, jsondata);
+        }
+    });
+};
+
+var getUserinfo = function (access_token, openid, cb) {
+    request.get({url:"https://api.weixin.qq.com/sns/userinfo?access_token="+access_token+"&openid="+openid+"&lang=zh_CN ",form:{}}, function (error, response, body) {
+        if (!error && response.statusCode == 200) {
+            var userinfo = JSON.parse(body);
+            cb(err, userinfo);
+        }
+    });
 };
 module.exports = router;
