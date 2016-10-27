5129afbd6591e632e6cfe9d7ce4e5cad6eba0aec
Houser <houserqu@qq.com>
2016-10-27 16:32:30 +0800

    微信授权登录

--- routes/wechatRouter.js
+++ routes/wechatRouter.js
@@ -1,5 +1,5 @@
 var express = require('express');
-var https = require("https");
+var request = require("request");
 var router = express.Router();
 
 var wechatconfig = require("../config/wechat");
@@ -17,18 +17,26 @@ router.get("/login", function (req, res, next) {
     console.log("code:" + code);
     console.log("state:" + state);
 
+    //微信获取用户资料第二步通过code换取网页授权access_token
     var url = "https://api.weixin.qq.com/sns/oauth2/access_token?appid=wx86caab40dba425ba&secret=d4624c36b6795d1d99dcf0547af5443d&code=" + code + "&grant_type=authorization_code"
-    https.get(url, function (redata) {
-        redata.on("data", function (d) {
-            console.log(d);
-            https.get("https://api.weixin.qq.com/sns/userinfo?access_token="+d.access_token+"&openid="+d.openid+"&lang=zh_CN ", function (info) {
-                info.on("data",function (userinfo) {
-                    console.log(userinfo);
-                });
+    request.get({url:url ,form:{}}, function (error, respon, body) {
+        if (!error && response.statusCode == 200) {
+            console.log(body);
+            var jsondata = JSON.parse(body);
+
+            //第四步拉取用户信息(需scope为 snsapi_userinfo)
+            request.get({url:"https://api.weixin.qq.com/sns/userinfo?access_token="+jsondata.access_token+"&openid="+jsondata.openid+"&lang=zh_CN ",form:{}}, function (error, response, body) {
+                if (!error && response.statusCode == 200) {
+                    var userdata = JSON.parse(body);
+                    console.log(userdata);
+                    res.end(JSON.stringify(userdata));
+                }else{
+                    res.end("get userinfo error");
+                }
             });
-        });
-    }).on('error', function (e) {
-        console.error(e);
+        }else{
+            res.end("get access_token error");
+        }
     });
 });
 
