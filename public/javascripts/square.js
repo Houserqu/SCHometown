e3406b73409d806b377c772517573a6cb7a30ec1
Houser <houserqu@qq.com>
2016-11-25 13:53:32 +0800

    完善基本信息修改

--- public/javascripts/square.js
+++ public/javascripts/square.js
@@ -156,7 +156,7 @@ $(document).ready(function () {
 
                             time = reverseTime(redata.weibolist[i].origintime);
 
-                            html += '<div class="weui_panel weui_panel_access"><div class="weui_panel_hd">' + redata.weibolist[i].nickname + '发表动态</div><div class="weui_panel_bd"> <div class="weui_media_box weui_media_text weibocontent"> <p class="weui_media_desc">' + redata.weibolist[i].content + '</p> <div class="weibocontent_imgs">' + imghtml + '<div style="clear: both"></div> </div> <ul class="weui_media_info"> <li class="weui_media_info_meta">' + time + '</li></ul> </div> </div> </div>'
+                            html += '<div class="weui_panel weui_panel_access"><a href="/weibodetail/'+redata.weibolist[i].idweibo+'"><div class="weui_panel_hd">' + redata.weibolist[i].nickname + '发表动态</div><div class="weui_panel_bd"> <div class="weui_media_box weui_media_text weibocontent"> <p class="weui_media_desc">' + redata.weibolist[i].content + '</p> <div class="weibocontent_imgs">' + imghtml + '<div style="clear: both"></div> </div> <ul class="weui_media_info"> <li class="weui_media_info_meta">' + time + '</li></ul> </div> </div> </div>'
                         }
                         $("#friends-weibo").append(html);
                         $('#friends-weibo').attr('data-state', 1);
